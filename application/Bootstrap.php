8b0ebaff7bae6f4fb8b3890844c9f7ebf5908448
03424754102 <03424754102@etr4322.fnde.gov.br>
2012-06-13 17:43:09 -0300

    [PATCH] CONFIG.INI = CONFIGURAÇÃO DE DB E BOOTSTRAP

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,6 +2,19 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    
+
+    protected function _initDb(){
+       $config =  $this->getOption('resources');
+
+       $db = Zend_Db::factory((string)$config['db']['adapter'],(array)$config['db']['params']);
+       Zend_Db_Table::setDefaultAdapter($db);
+       Zend_Registry::set('db', $db);
+
+    }
+
+
+
 
 
 }
