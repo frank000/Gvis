446e0c2bcd08d32c7e3e9327b10842e867fb9bfc
03424754102 <03424754102@etr4322.fnde.gov.br>
2012-06-14 10:06:27 -0300

    [PATCH] testes

--- tests/application/controllers/IndexControllerTest.php
+++ tests/application/controllers/IndexControllerTest.php
@@ -8,7 +8,37 @@ class IndexControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
         $this->bootstrap = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
         parent::setUp();
     }
-
+    
+    /**
+* URL Helper
+*
+* @param array $urlOptions
+* @param string $name
+* @param bool $reset
+* @param bool $encode
+*/
+    public function url($urlOptions = array(), $name = null, $reset = false, $encode = true)
+    {
+        $frontController = $this->getFrontController();
+        $router = $frontController->getRouter();
+        if (!$router instanceof Zend_Controller_Router_Rewrite) {
+            throw new Exception('This url helper utility function only works when the router is of type Zend_Controller_Router_Rewrite');
+        }
+        if (count($router->getRoutes()) == 0) {
+            $router->addDefaultRoutes();
+        }
+        return $router->assemble($urlOptions, $name, $reset, $encode);
+    }
+public function urlizeOptions($urlOptions, $actionControllerModuleOnly = true)
+    {
+        $ccToDash = new Zend_Filter_Word_CamelCaseToDash();
+        foreach ($urlOptions as $n => $v) {
+            if (in_array($n, array('action', 'controller', 'module'))) {
+                $urlOptions[$n] = $ccToDash->filter($v);
+            }
+        }
+        return $urlOptions;
+    }
     public function testIndexAction()
     {
         $params = array('action' => 'index', 'controller' => 'Index', 'module' => 'default');
