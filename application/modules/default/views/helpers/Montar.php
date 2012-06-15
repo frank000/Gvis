10a4c154a9a60c268a5f83735b6c8c03dfcfd826
03424754102 <03424754102@etr4322.fnde.gov.br>
2012-06-15 14:52:10 -0300

    [PATCH] Itens do menu

--- application/modules/default/views/helpers/Montar.php
+++ application/modules/default/views/helpers/Montar.php
@@ -12,9 +12,13 @@
  */
 class Zend_View_Helper_Montar extends Zend_View_Helper_Abstract{
     
+    private $_acl;
+    private $_auth;
+    
     public function montar(array $itensMenu = null){
-      
-        $acl = new Zend_Acl();
+        $this->_acl = new Zend_Acl();
+        $this->_auth = Zend_Auth::getInstance();
+    
         $menu = "<div>Menu<br/>";
          $menu .= "<ul id='cssdropdown'>";
         foreach ($itensMenu as $label => $item) {
@@ -38,6 +42,18 @@ class Zend_View_Helper_Montar extends Zend_View_Helper_Abstract{
         }
          $menu .= "</ul ></div>";
         echo  $menu;
+    }
+    
+    protected function autorizaUriPelaAcl($uri)
+    {
+        
+        $identidade = $this->_auth->getIdentity();
+        $arUri = explode('/', $uri);
+        if($this->_acl->allow($identidade->role, $arUri[1], $arUri[2]))
+        {
+            return true;
+        }
+        return false;
     }
 }
 
